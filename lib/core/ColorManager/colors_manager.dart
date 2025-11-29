8571e9263a0e52b14473a0071312178590f7732d
kassem <kassemdiaa537@gmail.com>
2025-11-29 15:27:54 +0200

    colors change

--- lib/core/ColorManager/colors_manager.dart
+++ lib/core/ColorManager/colors_manager.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ColorsManager {
-  static const Color redd = Color(0xFFE82626);
+  static const Color red = Color(0xFFE82626);
   static const Color black = Color(0xFF121312);
   static const Color white = Color(0xFFFFFFFF);
   static const Color yellow = Color(0xFFF6BD00);
