872d889e77326e8ba6f3a1f6ea9a991f52770358
kassem <kassemdiaa537@gmail.com>
2025-11-29 15:02:31 +0200

    modify homeScreen

--- lib/features/main_layout/Home_Screen/HomeScreen.dart
+++ lib/features/main_layout/Home_Screen/HomeScreen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/core/ColorManager/colors_manager.dart';
 import 'package:movies_app/core/imageManager/imageManager.dart';
+import 'package:movies_app/features/main_layout/Browse_Tap/browse.dart';
+import 'package:movies_app/features/main_layout/Home_Tap/home.dart';
+import 'package:movies_app/features/main_layout/Profile_Tap/profile.dart';
+import 'package:movies_app/features/main_layout/Search_Tap/search.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,16 +15,21 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int selectedIndex = 0;
+  int tapindex = 0;
+
+  List<Widget> taps = [
+    const HomeTap(),
+    const SearchTap(),
+    const BrowseTap(),
+    const ProfileTap(),
+  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         bottomNavigationBar: _BottomNavigationBar(),
         backgroundColor: Colors.transparent,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [Image.asset(Imagemanager.mainLayoutTitleImage)],
-        ),
+        body: taps[tapindex],
       ),
     );
   }
@@ -29,20 +38,20 @@ class _HomeScreenState extends State<HomeScreen> {
   _BottomNavigationBar() {
     return BottomNavigationBar(
       onTap: (currentIndex) {
-        selectedIndex = currentIndex;
+        tapindex = currentIndex;
         setState(() {});
       },
-      currentIndex: selectedIndex,
-      landscapeLayout: BottomNavigationBarLandscapeLayout.centered,
+      currentIndex: tapindex,
+
       type: BottomNavigationBarType.fixed,
       selectedIconTheme: IconThemeData(color: ColorsManager.yellow),
       unselectedIconTheme: IconThemeData(color: ColorsManager.white),
       backgroundColor: ColorsManager.gray,
       items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: ""),
-        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ""),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: ""),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
       ],
     );
   }
